c3f44097adcff298d137f16801e7cb79f273a819
Maryam Hemmati <Maryam.h@wearesolved.de>
2024-02-21 19:24:06 +0330

    crud user services

---
+++ DataSample.Application/Services/Users/Queries/GetUsers/GetUsersService.cs
@@ -0,0 +1,41 @@
+﻿using DataSample.Application.Interfaces.Contexts;
+using DataSample.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataSample.Application.Services.Users.Queries.GetUsers
+{
+    public class GetUsersService : IGetUsersService
+    {
+        private readonly IDataBaseContext _context;
+        public GetUsersService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+
+        public ReslutGetUserDto Execute(RequestGetUserDto request)
+        {
+            var users = _context.Users.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(request.SearchKey))
+            {
+                users = users.Where(p => p.FullName.Contains(request.SearchKey) && p.Email.Contains(request.SearchKey));
+            }
+            int rowsCount = 0;
+            var usersList= users.Select(p => new GetUsersDto
+            {
+                Email = p.Email,
+                FullName = p.FullName,
+                Id = p.Id,
+                IsActive=p.IsActive
+            }).ToList();
+
+            return new ReslutGetUserDto
+            {
+                Rows = rowsCount,
+                Users = usersList,
+            };
+        }
+    }
+}
